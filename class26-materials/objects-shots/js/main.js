09021e8cdb469244c4e07c4e93d2302946ad9f42
Evan Galt <evan.galt@gmail.com>
2023-05-11 11:33:58 -0500

    add simple cocktail app fetch request

--- class26-materials/objects-shots/js/main.js
+++ class26-materials/objects-shots/js/main.js
@@ -1,1 +1,17 @@
 //The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+document.querySelector('button').addEventListener('click', getDrink);
+
+function getDrink() {
+    const drink = document.querySelector('input').value;
+    fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+        .then(res => {
+            res.json();
+            console.log(res)
+        })
+        .then(data => {
+            console.log(data)
+        })
+        .catch(err => {
+            console.log(err)
+        });
+}
